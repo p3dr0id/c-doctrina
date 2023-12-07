841f54a68113d9d18739bc8bfeb1a7b4d242ca66
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2023-12-07 03:33:46 +0000

    atualização modulo05

--- modulo05-intermediario-p1/aula0-incremento_decremento.c
+++ modulo05-intermediario-p1/aula0-incremento_decremento.c
@@ -14,7 +14,7 @@ Data 05/12/2023
 /* Função Principal */
 
 int main() {
-    int x, y, a, b;
+    int x, y, a, b, c, d;
     x = 1;
     y = 1;
     x++; /* incrementando x de 1 com o operador aplicado na direita*/
@@ -30,8 +30,40 @@ int main() {
     printf("Valor de x após outro incremento:  %d\n", x);
     printf("Valor de y após outro incremento: %d\n", y);
 
+    c = y--;
+    d = y;
     printf("Valor de x após um decremento:  %d\n", --x);
-    printf("Valor de y atual: %d e também após um decremento: %d\n", y--, y);
+    printf("Valor de y atual: %d e também após um decremento: %d\n", c, d);
+
+    /*
+    Exercício proposto: projete um código em C para receber a entrada de um número
+    inteiro positivo de 16 bits (deverá ser compatível em máquinas diferentes) e o 
+    sistema calcule:
+    - O valor do número somado a 1;
+    - O valor do número subtraido em 1;
+    - A soma de todos os valores envolvidos;
+    - O quadrado da soma de todos os valores envolvidos;
+    O sistema deve apresentar o limite de valor igual a 85. Caso o usuário digite um valor maior
+    que 85, deverá gerar uma mensagem de erro. 
+    */
+    
+    unsigned short int n0, n1, n2, n3, n4; /* garante um inteiro de 2 bytes (16 bits) 0 a 65535 */
+    printf("Digite um número inteiro positivo menor que 85: ");
+    scanf("%hu", &n0); /* o hu faz referência ao tipo unsigned short int*/
+
+    if (n0 > 85)
+        printf("Você digitou um número fora do intervalo permitido!");
+    else {
+        n1 = n0++; /* atribui o valor do número digitado n0 a variável "n1" e incrementa o número*/
+        printf("O valor digitado foi: %d\n", n1); /* mostra o valor digitado */
+        printf("O valor do número somado de 1 é: %d\n", n0); /* mostra a valor digitado incrementado */
+        n2 = n1--; /* armazena o valor digitado em n2 (original) e decrementa o valor digitado em 1*/
+        printf("O valor do número subtraído de 1 é: %d\n", n1);
+        n3 = n0 + n1 + n2; /* soma de todos os valores envolvidos */
+        printf("A soma de todos os valores envolvidos é: %d\n", n3);
+        n4 = n3 * n3;
+        printf("O quadrado da soma de todos os valores envolvidos é: %d\n", n4);
+    }
 
 
     return 0;
---
+++ modulo05-intermediario-p1/aula1-laco_while.c
@@ -0,0 +1,21 @@
+/* ===========================================================================================
+
+Titulo da aula: laço while
+
+Autor: Pedro Dias Pinto
+Data 07/12/2023
+
+==============================================================================================*/
+
+
+
+/* Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Função Principal */
+
+int main() {
+
+    return 0;
+} /* end main */

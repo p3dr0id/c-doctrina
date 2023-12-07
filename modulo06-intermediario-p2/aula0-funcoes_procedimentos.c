5f21db4fb1f99aaab094c8b50fbde4a46eb174ed
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2023-12-07 18:10:09 +0000

    atualização módulo 6

---
+++ modulo06-intermediario-p2/aula0-funcoes_procedimentos.c
@@ -0,0 +1,19 @@
+/* ===========================================================================================
+
+Titulo da aula: Funções e Procedimentos
+
+Autor: Pedro Dias Pinto
+Data 07/12/2023
+
+==============================================================================================*/
+
+/* Bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Função Principal */
+
+int main() {
+
+    return 0;
+} /* end main */
